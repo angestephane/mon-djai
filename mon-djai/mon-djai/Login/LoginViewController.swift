2b252dbaba6686db5c98b2558db74be31b5b6e7a
Boguhe Ange Stephane <stephane.boguhe@babelgroup.com>
2023-07-23 20:17:35 +0100

    setup the project and start the login view.

---
+++ mon-djai/mon-djai/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  mon-djai
+//
+//  Created by user.local on 23/7/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
