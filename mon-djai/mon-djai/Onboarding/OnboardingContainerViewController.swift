a2d05ebd852de01c8473df4fc8d01f548e5d29d5
Stephane Boguhe <boguhestephane97@gmail.com>
2023-07-26 00:11:18 +0100

    Set close button view

--- mon-djai/mon-djai/Onboarding/OnboardingContainerViewController.swift
+++ mon-djai/mon-djai/Onboarding/OnboardingContainerViewController.swift
@@ -11,10 +11,8 @@ class OnboardingContainerViewController: UIViewController {
 
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-        }
-    }
+    var currentVC: UIViewController
+    var closeButton = UIButton(type: .system)
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
@@ -38,7 +36,12 @@ class OnboardingContainerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setup()
+        style()
+        layout()
+    }
+    
+    private func setup() {
         view.backgroundColor = .systemTeal
         
         addChild(pageViewController) // Add view controller inside parent view controller.
@@ -57,6 +60,26 @@ class OnboardingContainerViewController: UIViewController {
         
         pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
         currentVC = pages.first!
+    }
+    
+    private func style() {
+        
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("Fermer", for: [])
+        closeButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title3)
+        closeButton.titleLabel?.textColor = .black
+        closeButton.addTarget(self, action: #selector(closeTape), for: .primaryActionTriggered)
+        
+        view.addSubview(closeButton)
+        
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
+        ])
+        
     }
 }
 
@@ -89,5 +112,14 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
 
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+}
+
+// MARK: - Actions
+
+extension OnboardingContainerViewController {
+    
+    @objc func closeTape(_ sender: UIButton) {
+        // TODO
     }
 }
